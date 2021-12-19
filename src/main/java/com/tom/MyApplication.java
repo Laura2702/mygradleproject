8aff934039946c49a7f1190b2f1ba4681448b79e
Laura2702 <l.pichlmeier@gmx.de>
2021-12-19 08:38:45 +0100

    Initial commit

---
+++ src/main/java/com/tom/MyApplication.java
@@ -0,0 +1,7 @@
+package com.tom;
+
+public class MyApplication {
+    public static void main(String[] args) {
+        System.out.println("Meine erste Gradle Application!");
+    }
+}
---
+++ src/test/java/com/tom/MyApplicationTest.java
@@ -0,0 +1,10 @@
+package com.tom;
+
+import org.junit.Test;
+
+public class MyApplicationTest {
+    @Test
+    public void testSomething() {
+        MyApplication.main(new String[]{});
+    }
+}
